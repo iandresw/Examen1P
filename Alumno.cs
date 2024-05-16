22020ffc5ed477362850e78d2d00d36b3f757628
iandresw <rbejarano@unah.hn>
2024-05-15 19:49:05 -0600

    Clase Alumno
    Contiene tres propiedades  nombre alumno, numero de cuenta y el correo electronico

---
+++ Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1P
+{
+    public class Alumno
+    {
+        public string NombreAlumno { get; set; }
+
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
+    }
+}
