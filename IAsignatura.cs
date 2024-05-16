cd997f5b272b8686ff7b39db309e65d7fe06a09d
iandresw <rbejarano@unah.hn>
2024-05-15 22:05:13 -0600

    Create IAsignatura.cs
    Se crearon los metodos CalcularNotaFinal con tres parametros y CalcularNotaFinal  sin parametros y MensajeNotaFinal con el parametro double

---
+++ IAsignatura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1P
+{
+    public interface IAsignatura
+    {
+        public double CalcularNotaFinal();
+
+        public double CalcularNotaFinal(double n1, double n2, double n3);
+
+        public string MensajeNotaFinal(double NotaFinal);
+    }
+}
